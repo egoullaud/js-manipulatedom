0a1c0b58150b0efc5a84a844a9c3676730d9e25b
elizabeth.goullaud <egoullaud@gmail.com>
2022-08-09 12:04:20 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// var firstListItem = document.querySelector("ul li");
+// firstListItem.style.textTransform = "uppercase";
+// console.log(firstListItem);
+
+// var moreReasons = document.querySelector(".more-reasons");
+// moreReasons.style.fontSize = "2.5em";
+// console.log(moreReasons);
+
+// var whyJS = document.querySelector(".intro h3");
+// whyJS.innerHTML = 'Why learn <span class="highlight"> JavaScript</span>?';
+// var highlight = document.querySelector(".highlight");
+// highlight.style.backgroundColor = "#FFFF82";
+// console.log(whyJS);
+
+var mainImage = document.querySelector("img");
+mainImage.src = "../img/js-code.png";
+mainImage.alt = "Javascript code exmaple";
+console.log(mainImage);
